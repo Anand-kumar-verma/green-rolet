e282aa795e79979bb12e3886e32666db15914bba
anand <vermaanand278@gmail.com>
2024-08-10 15:21:41 +0530

    dsaf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './assets/css/main.css';
-import RolletIFrame from './component/RolletIFrame';
-import Login from './pages/Auth/Login';
-import Home from './pages/Home/Home';
+import { Route, Routes } from "react-router-dom";
+import "./assets/css/main.css";
+import RolletIFrame from "./component/RolletIFrame";
+import Login from "./pages/Auth/Login";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' Component={Login}></Route>
-      <Route path='/dashboard' Component={Home}></Route>
-      <Route path='/rollet' Component={RolletIFrame}></Route>
+      <Route path="/" Component={Login}></Route>
+      <Route path="/dashboard" Component={Home}></Route>
+      <Route path="/rollet" Component={RolletIFrame}></Route>
     </Routes>
   );
 }
